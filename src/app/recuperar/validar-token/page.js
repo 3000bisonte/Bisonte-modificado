0f7182bbe4877d47dda925a6ff91df1317baac1b
YesicaCausado <yesica.causadoa@uniagustiniana.edu.co>
2025-07-14 17:00:00 -0500

    cambios

--- src/app/recuperar/validar-token/page.js
+++ src/app/recuperar/validar-token/page.js
@@ -77,7 +77,7 @@ export default function ValidarToken() {
       if (res.ok && data.ok) {
         setMsg("¡Contraseña actualizada exitosamente!");
         setTimeout(() => {
-          router.push("/recuperar/exito");
+          router.push("recuperar/exito");
         }, 2000);
       } else {
         console.error('❌ Error del servidor:', data.error);
@@ -136,6 +136,7 @@ export default function ValidarToken() {
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   <svg className="h-5 w-5 text-slate-400" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                  </svg>
                 </div>
                 <input
                   id="email"
@@ -157,6 +158,7 @@ export default function ValidarToken() {
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   <svg className="h-5 w-5 text-slate-400" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1221.75 8.25z" />
+                  </svg>
                 </div>
                 <input
                   id="token"
